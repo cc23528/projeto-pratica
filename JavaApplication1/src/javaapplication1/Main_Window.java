6012f44f6a5855a781ea8e9c971ea03d8fdc3c12
cc23531 <cc23531@g.unicamp.br>
2023-11-22 22:01:29 -0300

    Update Main_Window.java

--- JavaApplication1/src/javaapplication1/Main_Window.java
+++ JavaApplication1/src/javaapplication1/Main_Window.java
@@ -20,7 +20,6 @@ import javax.swing.ImageIcon;
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 import javax.swing.filechooser.FileNameExtensionFilter;
-import com.toedter.calendar.JDateChooser;
 
 
 /**
