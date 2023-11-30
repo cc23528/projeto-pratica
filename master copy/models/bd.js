ed5b1f9134e40116f66fe4700f92249477277dd7
Verissimo <cc23528@g.unicamp.br>
2023-11-29 22:28:45 -0300

    att2
    att2

--- master copy/models/bd.js
+++ master copy/models/bd.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('bd23528', 'bd23528', 'Escola@123', {
+const sequelize = new Sequelize('bd23528', 'bd23528', '12345', {
   host: 'db4free.net',
   dialect: 'mysql'
 });
